93e10d788605910ff28881d2060b896916c7bd15
Engström <joel.engstrom@if.se>
2023-03-22 22:13:35 +0100

    a

--- netlify/functions/stripe.js
+++ netlify/functions/stripe.js
@@ -3,18 +3,18 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 console.log("running");
 exports.handler = async (event, context) => {
-  const { quantity } = JSON.parse(event.body);
+  /* const { quantity } = JSON.parse(event.body); */
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     line_items: [
       {
         price: "price_1MoXwGLHXKTpG87XbhFcS6CW",
-        quantity: quantity, // use the quantity value from the request body
+        quantity: 3, // use the quantity value from the request body
       },
     ],
     mode: "payment",
-    success_url: "https://serverless-payments.netlify.app/success",
-    cancel_url: "https://serverless-payments.netlify.app/cancel",
+    success_url: "https://lovnasbryggeri.netlify.app/sucess.html",
+    cancel_url: "https://lovnasbryggeri.netlify.app/folkölshoppen.html",
   });
   return {
     statusCode: 200,
